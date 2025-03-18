7dcee0350f38846606d613eb79f239f196c4a177
schminho <77048995+schminho@users.noreply.github.com>
2025-03-18 10:26:36 +0100

    deete stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# ptrsmd
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/schminho/ptrsmd)
--- src/config/hero.ts
+++
@@ -1,7 +0,0 @@
-// Using public URLs for assets
-export const heroConfig = {
-  backgroundVideo: '/assets/videos/niko.mp4',
-  fallbackImage: '/assets/images/hero-globe.avif',
-  title: 'peterschmidt',
-  subtitle: 'Just some dude interested in tech stuff'
-};
