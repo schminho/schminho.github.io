fb9db0a76473e276b7843632911ec723f0ba2196
schminho <77048995+schminho@users.noreply.github.com>
2025-03-19 09:18:15 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,8 @@ export function Header() {
   const location = useLocation();
 
   const navItems = [
-    { href: "#about", icon: User, label: "About" },
-    { href: "#contact", icon: Mail, label: "Contact" }
+    { href: "/about", icon: User, label: "About" },
+    { href: "/contact", icon: Mail, label: "Contact" }
   ];
 
   useEffect(() => {
