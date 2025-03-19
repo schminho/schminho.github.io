c84cd95ba596fc0b6da4b32b005091328b612e63
schminho <77048995+schminho@users.noreply.github.com>
2025-03-19 09:12:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
-import { ProjectsPage } from './pages/ProjectsPage';
-import { ProjectConvertPage } from './pages/ProjectConvertPage';
-import { AboutPage } from './pages/AboutPage';
 
 function App() {
   return (
@@ -13,9 +10,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/projects/convert" element={<ProjectConvertPage />} />
-          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
     </Router>
